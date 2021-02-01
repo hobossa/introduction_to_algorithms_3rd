8d1b9820c48cc0bf59cf52963eff7ca338f31f2f
hobossa <hoboss@gmail.com>
2021-02-01 19:51:17 +0800

    Update SelectionSort.java

--- khan/src/main/java/SelectionSort.java
+++ khan/src/main/java/SelectionSort.java
@@ -4,7 +4,7 @@ public class SelectionSort {
     public static <T> void sort(T[] array, Comparator<T> comp) {
         for (int i = 0; i < array.length; i++) {
             int n = i;
-            for (int j = i; j < array.length; j++) {
+            for (int j = n+1; j < array.length; j++) {
                 if (comp.compare(array[j], array[n]) < 0) {
                     n = j;
                 }
